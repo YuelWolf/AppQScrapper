ee0678a448e39cb78d2094f379e8e21938831caf
Andrés Rodríguez <darkknight64@gmail.com>
2020-04-07 11:36:39 -0500

    scrapper working

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const cheerio = require('cheerio')
 const request = require('request-promise')
+var date = new Date()
+var hour = date.getHours()
 
 async function init(){
    const $ = await request({
        uri: 'http://201.245.192.252:81/Report/HourlyReports',
        transform: body => cheerio.load(body)
     })
-   const stationName = $('.TableStation').each((i,el)=>{
-      console.log($(el).find('h2').html().trim());   
-   })
-   //const dataStation = $('.TableStation').find('td')
-   //console.log(dataStation.text().trim());
+   const stationName = await $('.TableStation').each((i,el)=>{
+      console.log($(el).find('h2').html().trim())
+      $(el).find('td').each((i, el) =>{
+         if($(el).text()!="")
+         console.log($(el).text().trim());         
+      })      
+    })
+    console.log(hour);
+    
 }
 
 init()
